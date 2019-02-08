a6e38da07f7a093238a6884b85603c493d064564
kevinahumada7 <47418081+kevinahumada7@users.noreply.github.com>
2019-02-08 12:19:34 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+    var edad;
+    edad= document.getElementById("edad").value;
+    if(15==edad)
+        {
+            alert("Niña Bonita");
+        }
+    else
+        {
+            alert("Error");
+        }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+edad= document.getElementById("edad").value;
+if(edad>=13 && edad<=17)
+    {
+        alert("Eres un adolecente")
+    }
+else
+    {
+        alert("Error")
+    }
 
 }//FIN DE LA FUNCIÓN

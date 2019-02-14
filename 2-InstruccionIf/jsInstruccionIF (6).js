44889f863c7a14e9c59a587b1bbb912b670638e2
kevinahumada7 <kevinahumada7@gmail.com>
2019-02-13 23:33:41 -0300

    ejercicios 6 y 7

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
+    edad=document.getElementById("edad").value;
 
+        if (edad>=18) {
+            alert("Eres mayor de edad");
+        }   else if (edad>=13 && edad<=17) {            
+                alert("Eres un adolecente");
+            }   else if (edad<=13) {
+                    alert("Eres un niño");
+                }
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad, estadoCivil;
+    edad= document.getElementById("edad").value;
+    estadoCivil= document.getElementById("estadoCivil");
+        if (edad<18 && estadoCivil.selectedIndex!=0) {
+            alert("Eres muy pequeño para NO ser soltero")
+        }
+        else {
+            alert("Es normal")
+        }
 
-	
 
 
 }//FIN DE LA FUNCIÓN

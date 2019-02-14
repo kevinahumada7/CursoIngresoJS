2df532e05f3f5744b9937c964c971e288f691054
kevinahumada7 <kevinahumada7@gmail.com>
2019-02-13 21:46:32 -0300

    Arreglos en Recuperatorios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,7 +1,4 @@
 function mostrar()
 {
-  var nombre, localidad;
-  nombre= document.getElementById("elNombre").value;
-  localidad= document.getElementById("laLocalidad").value;
-  alert("Usted es "+nombre+" y vive en la localidad de "+localidad)
+  
 }

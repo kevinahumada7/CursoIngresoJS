d5a458d5a1a08b7ef9fe695d4567bc4b71c228f9
Kevin Ahumada <kevinahumada7@gmail.com>
2019-02-22 10:51:22 -0300

    SimulacroDeParcial

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,61 @@
 function mostrar()
 {
+    var importeUno;
+    var importeDos;
+    var importeTres;
+    var importeCuatro;
+    var ImporteMayor;
+    var precioTotal;
+    var descuento;
+    var precioFinal;
 
+        importeUno=prompt("Ingresar 1er Importe");
+        importeDos=prompt("Ingresar 2do Importe");
+        importeTres=prompt("Ingresar 3er Importe");
+        importeCuatro=prompt("Ingresar 4to Importe");
+
+    importeUno=parseInt(importeUno);
+    importeDos=parseInt(importeDos);
+    importeTres=parseInt(importeTres);
+    importeCuatro=parseInt(importeCuatro);
+
+    precioTotal=importeUno+importeDos+importeTres+importeCuatro;
+
+    if(precioTotal>=100)
+    {
+        descuento=0.90;
+    }else
+    {
+        if(precioTotal>=50)
+        {
+            descuento=0.95;
+        }else
+        { 
+            if(precioTotal<50)
+            {
+                descuento=1.15;
+            }
+        }
+    }
+    if (importeUno>=importeDos && importeUno>=importeTres && importeUno>=importeCuatro){
+        ImporteMayor=importeUno;
+    }else{
+        if(importeDos>=importeTres && importeDos>=importeCuatro)
+        {
+            ImporteMayor=importeDos;
+        }else
+        {
+            if(importeTres>=importeCuatro)
+            {
+                ImporteMayor=importeTres
+            }else
+            {
+                ImporteMayor=importeCuatro
+            }
+        }
+    }
+
+    precioFinal=precioTotal*descuento;
+    
+    alert("El importe mayor es $"+ImporteMayor+"\nEl precio total es $"+precioTotal+"\nEl precio final es $"+precioFinal);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,57 @@
 function mostrar()
 {
+    var precioHabitacion;
+    var metodoDePago;
+    var paquete;
+    var descuentoDePaquete;
+    var descuentoDeTarjeta;
+    var descuentoTotal;
+    var precioFinal;
 
+        precioHabitacion= prompt("Ingresar precio de la habitación:");
+        precioHabitacion= parseInt(precioHabitacion);
+        metodoDePago= prompt("Ingresar metodo de pago entre las siguientes tarjetas:\n Visa\n Paypal\n MercadoPago\n Efectivo\n Otros Medios");
+        paquete= prompt("Ingresar paquete:\n todoIncluido o todoDesayuno");
+
+    switch(metodoDePago)
+    {
+        case "Visa":
+            descuentoDeTarjeta=0.90;
+        break;
+
+        case "PayPal":
+            descuentoDeTarjeta=0.85;
+            if (paquete="todoIncluido")
+            {
+                descuentoDePaquete=0.10;
+            }
+        break;
+
+        case "MercadoPago":
+            descuentoDeTarjeta=0.90;
+        break;
+
+        case "Efectivo":
+            descuentoDeTarjeta=0.80;
+            if (paquete="todoIncluido")
+            {
+                descuentoDePaquete=0.15;
+            }else
+            {
+                if(paquete="soloDesayuno")
+                {
+                    descuentoDePaquete=0.10;
+                }
+            }
+        break;
+
+        default:
+            descuentoDeTarjeta=0.95;
+        break;
+    }
+
+    descuentoTotal=descuentoDeTarjeta+descuentoDePaquete;
+    precioFinal=precioHabitacion*descuentoTotal;
+    
+    alert("El precio de la habitacion es $"+precioHabitacion+"\nEl descuento por usar la tarjeta "+metodoDePago+" y elejir el paquete "+paquete+" es: "+descuentoTotal*100+"%\nEl precio final es $"+precioFinal);
 }

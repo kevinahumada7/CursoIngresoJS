d648f5c1444830d2d2256fa466d9100af2d269c8
Kevin Ahumada <kevinahumada7@gmail.com>
2019-02-22 12:21:51 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	alert('iteración while');
+	
+	var contador;
+		contador=10;
+
+	while(contador>0)
+	{
+		alert(contador--);
+	}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
+    var clave = prompt("ingrese el número clave.");
+    var intentos
+        intentos=0
+    while(clave!="utn750" && intentos<2)
+    {
+        clave=prompt("Ingrese el numero clave");
+        ++intentos;
+            alert("Ha excedido la cantidad de intentos")
+    }
 
 }//FIN DE LA FUNCIÓN
